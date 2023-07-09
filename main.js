6956eb595f3269bd6492689160041edb497a825d
aadarsh <aadarshraghuwanshi91@gmail.com>
2023-07-09 12:39:59 +0530

    ok

---
+++ main.js
@@ -0,0 +1,278 @@
+
+
+
+
+
+
+
+
+        jQuery(document).on('click', function(e){
+            let myTargetElement = e.target;
+            let selector, mainElement;
+            if(jQuery(myTargetElement).hasClass('search-toggle') || jQuery(myTargetElement).parent().hasClass('search-toggle') || jQuery(myTargetElement).parent().parent().hasClass('search-toggle') ){
+                if(jQuery(myTargetElement).hasClass('search-toggle')) {
+                    selector = jQuery(myTargetElement).parent();
+                    mainElement = jQuery(myTargetElement);
+                } else if (jQuery(myTargetElement).parent().hasClass('search-toggle')){
+                    selector = jQuery(myTargetElement).parent().parent();
+                    mainElement = jQuery(myTargetElement).parent();
+                }else if (jQuery(myTargetElement).parent().parent().hasClass('search-toggle')){
+                    selector = jQuery(myTargetElement).parent().parent().parent();
+                    mainElement = jQuery(myTargetElement).parent().parent();
+                }
+                if(!mainElement.hasClass('active') && jQuery('.navbar-list li').find('.active')){
+                    jQuery('.navbar-right li').removeClass('.iq-show');
+                    jQuery('.navbar-right li .search-toggle').removeClass('active');
+                }
+
+                selector.toggleClass('iq-show');
+                mainElement.toggleClass('active');
+                e.preventDefault();
+            } else if (jQuery(myTargetElement).is('search-input')){} else {
+                jQuery('.navbar-right li').removeClass('.iq-show');
+                jQuery('.navbar-right li .search-toggle').removeClass('active');
+            }
+        });
+       
+        jQuery('.c-toggler').on("click", function(){
+            jQuery('body').addClass('nav-open');
+        });
+
+
+        $('#home-slider').slick({
+            autoplay : false,
+            speed : 800,
+            lazyload : 'progressive',
+            arrows : true,
+            dots : false,
+            prevArrow : '<div class="slick-nav prev-arrow"><i class="fa fa-chevron-right"></i></div>',
+            nextArrow : '<div class="slick-nav next-arrow"><i class="fa fa-chevron-left"></i></div>',
+            responsive : [
+                {
+                    breakpoint : 992,
+                    settings : {
+                        dots : true,
+                        arrows : false,
+                    }
+                }
+            ]
+        }).slickAnimation();
+        $(".slick-nav").on("click touch", function (e){
+            e.preventDefault();
+
+            var arrow = $(this);
+
+            if(!arrow.hasClass('animate')){
+                arrow.addClass('animate');
+                setTimeout(() => {
+                    arrow.removeClass('animate');
+                }, 1600);
+            }
+        });
+
+        jQuery('.favorites-slider').slick({
+            dots:false,
+            arrow : true,
+            infinite : true,
+            speed : 300,
+            autoplay : false,
+            slidesToShow : 4,
+            slidesToScroll :1,
+            nextArrow: '<a href="#" class="slick-arrow slick-next"><i class="fa fa-chevron-right"></i></a>',
+            prevArrow: '<a href="#" class="slick-arrow slick-prev"><i class="fa fa-chevron-left"></i></a>',
+            responsive : [
+                {
+                    breakpoint:1200,
+                    settings : {
+                        slidesToShow : 3,
+                        slidesToScroll : 1,
+                        infinite : true,
+                        dots : true
+                    }
+                },
+                {
+                    breakpoint:768,
+                    settings : {
+                        slidesToShow : 2,
+                        slidesToScroll : 1
+                    }
+                },
+                {
+                    breakpoint:480,
+                    settings : {
+                        slidesToShow : 1,
+                        slidesToScroll : 1
+                    }
+                },
+            ]
+        });
+
+        jQuery('#top-ten-slider').slick({
+            slidesToScroll : 1,
+            slidesToShow : 1,
+            arrows : false,
+            fade : true,
+            asNavFor : '#top-ten-slider-nav',
+            responsive : [
+                {
+                    breakpoint : 992,
+                    settings : {
+                        asNavFor : false,
+                        arrows : true ,
+                        nextArrow : '<button class="NextArrow"><i class="fa fa-angle-right"></i></button>',
+                        prevArrow : '<button class="PrevArrow"><i class="fa fa-angle-left"></i></button>',
+                    }
+                }
+            ]
+        });
+        jQuery('#top-ten-slider-nav').slick({
+            slidesToShow : 3,
+            slidesToScroll : 1,
+            asNavFor : '#top-ten-slider',
+            dots: false,
+            arrows : true,
+            infinite : true,
+            vertical : true,
+            verticalSwiping : true,
+            centerMode :false,
+            nextArrow : '<button class="NextArrow"><i class="fa fa-angle-down"></i></button>',
+            prevArrow : '<button class="PrevArrow"><i class="fa fa-angle-up"></i></button>',
+            focusOnSelect : true,
+            responsive : [
+                {
+                    breakpoint : 1200,
+                    settings : {
+                        slidesToShow : 2,
+                    }
+                },
+                {
+                    breakpoint : 600,
+                    settings : {
+                        asNavFor : false,
+                    }
+                },
+            ]
+        });
+        
+
+        jQuery("#trending-slider").slick({
+            slidesToShow : 1,
+            slidesToScroll : 1,
+            arrows : false,
+            fade : true,
+            draggable : false,
+            asNavFor : "#trending-slider-nav",
+        });
+
+        jQuery("#trending-slider-nav").slick({
+            slidesToShow : 5,
+            slidesToScroll : 1,
+            asNavFor : "#trending-slider",
+            dots : false ,
+            arrows : true ,
+            nextArrow: '<a href="#" class="slick-arrow slick-next"><i class="fa fa-chevron-right"></i></a>',
+            prevArrow: '<a href="#" class="slick-arrow slick-prev"><i class="fa fa-chevron-left"></i></a>',
+            infinite : true,
+            centerMode : true,
+            centerPadding : 0,
+            focusOnSelect : true,
+            responsive : [
+                {
+                    breakpoint : 1024,
+                    settings : {
+                        slidesToShow : 2,
+                        slidesToScroll : 1,
+                    }
+                },
+                {
+                    breakpoint : 600,
+                    settings : {
+                        slidesToShow : 1,
+                        slidesToScroll : 1,
+                    }
+                }
+            ]
+        });
+
+        jQuery('.episodes-slider1').owlCarousel({
+            loop : true,
+            margin : 20,
+            nav: true,
+            navText : ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i> "],
+            dots : false,
+            responsive : {
+                0:{
+                    items : 1
+                },
+                600: {
+                    items : 1
+                },
+                1000 : {
+                    items : 4
+                }
+            }
+        });
+
+
+        jQuery('.trending-content').each(function(){
+            var highestBox = 0;
+            jQuery('.tab-pane', this).each(function(){
+                if(jQuery(this).height() > highestBox){
+                    highestBox = jQuery(this).height();
+                }
+            });
+            jQuery('.tab-pane', this).height(highestBox);
+        });
+
+        if(jQuery('select').hasClass('season-select')){
+            jQuery('select').select2({
+                theme : 'bootstrap4',
+                allowClear : false,
+                width : 'resolve'
+            });
+        }
+        
+
+
+
+
+
+
+
+$('#audio-control').click(function(){
+    if( $("#myVideo").prop('muted') ) {
+          $("#myVideo").prop('muted', false);
+          $(this).text('🔉');
+      // or toggle class, style it with a volume icon sprite, change background-position
+    } else {
+      $("#myVideo").prop('muted', true);
+      $(this).text('🔇');
+    }
+});
+
+
+
+function search() {
+    const searchbox = document.getElementById("search-item").value.toUpperCase();
+    const storeitems = document.getElementById("acard-acontainer");
+    const product = document.querySelectorAll(".acard");
+    const pname = document.getElementsByTagName("h2");
+
+
+    for (var i = 0; i < pname.length; i++) {
+        let match = product[i].getElementsByTagName('h2')[0];
+
+        if (match) {
+            let textvalue = match.textcontent || match.innerHTML;
+
+            if (textvalue.toUpperCase().indexOf(searchbox) > -1) {
+                product[i].style.display = "";
+            } else {
+                product[i].style.display = "none";
+            }
+        }
+    }
+
+}
+
+
